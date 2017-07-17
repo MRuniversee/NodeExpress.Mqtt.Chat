592f0be07992e12d52322ac9711e7359bb654809
MRuniversee <renatobylyk@gmail.com>
2017-07-17 23:04:47 +0300

    v0.3

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express') // Express framework for node js
 const mosca = require('mosca'); // Mosca mqtt server
 const ascoltatori = require('ascoltatori'); //----
+const bodyParser = require('body-parser');
 const path = require('path'); // Path module for paths
 const app = express() // Init App
 
 var mymqtt = require('mqtt');
-
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
 app.set('views', path.join(__dirname, 'views'));
@@ -25,16 +26,27 @@ app.get('/getUsers', function(req, res) {
   res.json('This is my answer');
 });
 
+// Create User   ----------------------------------------------------------------------------
+app.get('/CreateUser', function(req, res) {
+  var client  = mymqtt.connect('mqtt://localhost');
+  res.set('Content-Type', 'application/javascript');
+});
+
 //  Start Server ----------------------------------------------------------------------------
 app.listen(3000, function() {})
 
 // Settings for Mqtt -------------------------------------------------------------------
 settings = {
   type: 'mqtt',
-  json: false,
+  //json: false,
   mqtt: require('mqtt'),
   host: '127.0.0.1',
-  port: 1883
+  port: 1883,
+  http: {
+    port: 80,
+    bundle: true,
+    static: './'
+  }
 };
 ascoltatori.build(settings, function(ascoltatore) {});
 
@@ -48,7 +60,7 @@ server.on('published', function(packet, client) {
 // fired when a client connects
 server.on('clientConnected', function(client) {
   console.log('Client Connected:', client.id);
-  
+
 });
 
 // fired when a client disconnects
