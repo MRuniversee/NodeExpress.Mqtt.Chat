f65b07235cc493f46b7afaae4c78635fcb9ea474
MRuniversee <renatobylyk@gmail.com>
2017-07-19 03:09:26 +0300

    messages added v0.38

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const path = require('path'); // Path module for paths
 const app = express() // Init App
 
 var mymqtt = require('mqtt');
+var keptOnlineUsers = [];
+var counter = 0 ;
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
@@ -17,13 +19,14 @@ app.set('view engine', 'pug');
 app.get('/', function(req, res) {
   res.render('index', {
     username: 'Default_'+ parseInt(Math.random() * 100, 10),
-    myMqttfunction : mymqtt
+    myMqttfunction : mymqtt,
+    theOnlineUsers : keptOnlineUsers.filter(function(e){return e})
   });
 });
 
 // Update User List  ----------------------------------------------------------------------------
 app.get('/getUsers', function(req, res) {
-  res.json('This is my answer');
+  res.json(keptOnlineUsers);
 });
 
 // Create User   ----------------------------------------------------------------------------
@@ -39,6 +42,7 @@ app.listen(3000, function() {})
 settings = {
   type: 'mqtt',
   //json: false,
+  clientId : 'mqtt_library',
   mqtt: require('mqtt'),
   host: '127.0.0.1',
   port: 1883,
@@ -55,17 +59,27 @@ var server = new mosca.Server(settings);
 
 // fired when a message is published
 server.on('published', function(packet, client) {
-  console.log('Published', packet.payload.toString());
+//  console.log('Published', packet.payload.toString());
 });
 // fired when a client connects
 server.on('clientConnected', function(client) {
   console.log('Client Connected:', client.id);
+   if(client.id !='mqtt_library'){
+     keptOnlineUsers[counter] = client.id;
+     counter++;
+   }
 
 });
 
 // fired when a client disconnects
 server.on('clientDisconnected', function(client) {
 //  console.log('Client Disconnected:', client.id);
+  for(var i=0;i<=counter;i++){
+    if(keptOnlineUsers[i]==client.id){
+      keptOnlineUsers[i] = null;
+      break;
+    }
+  }
 });
 
 server.on('ready', setup);
--- public/design/scripts/chatScript.js
+++ public/design/scripts/chatScript.js
@@ -1,47 +1,44 @@
 
 
-$(document).ready(function(){
-/*
-  var client = mqtt.createClient({
-    clientId:'Renato-From-Web',
-    clean : false
-  }).subscribe(clientId+'_room',{qos:1},function(){
-    alert('Client Connected');
-  }).close(function () {
-    alert('Client Disconnected');
-  }).connect('localhost:3000')
+	$(document).ready(function(){
 
-  setInterval(function(){
-    console.log("Hello");
-    }, 3000);
-*/
 	$('.chat_head').click(function(){
+
 		$('.chat_body').slideToggle('slow');
 	});
-	$('.msg_head').click(function(){
-		$('.msg_wrap').slideToggle('slow');
-	});
-
-	$('.close').click(function(){
-		$('.msg_box').hide();
-	});
-
-	$('.user').click(function(){
-
-		$('.msg_wrap').show();
-		$('.msg_box').show();
-	});
-
-	$('textarea').keypress(
-    function(e){
-        if (e.keyCode == 13) {
-            e.preventDefault();
-            var msg = $(this).val();
-						$(this).val('');
-						if(msg!='')
-						$('<div class="msg_b">'+msg+'</div>').insertBefore('.msg_push');
-						$('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
-        }
-    });
 
 });
+
+function sendMsg(id){
+	$('<div class="msg_box" id="'+id+'"style="right:290px; display:none;">'+
+			'<div class="msg_head" id="'+id+'" onclick="msgHead(\''+id+'\')" >'+ id +'<div class="close" id="'+id+'" onclick="msgClose(\''+id+'\')"> X </div> </div>'+
+			'<div class="msg_wrap" id="'+id+'"><div class="msg_body"><div class="msg_push"></div></div>'+
+					'<div class="msg_footer">'+
+							'<textarea id="'+id+'" onkeypress="sendText(\''+id+'\',event)" class="msg_input" rows="4"></textarea>'+
+				  '</div>'+
+			'</div>'+
+		'</div>').insertAfter('.chat_box');
+	$('.msg_wrap').show();
+	$('.msg_box').show();
+};
+
+
+function msgClose(id){
+	$('.msg_box#'+id).hide();
+}
+
+function msgHead(id){
+	$('.msg_wrap#'+id).slideToggle('slow');
+}
+
+function sendText(id,event){
+	if (event.keyCode == 13){
+		event.preventDefault();
+		var msg = $('.msg_input#'+id).val();
+		$('.msg_input#'+id).val('');
+		if(msg!=''){
+			$('<div class="msg_b">'+msg+'</div>').insertBefore('.msg_push');
+			$('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
+		}
+	}
+}
