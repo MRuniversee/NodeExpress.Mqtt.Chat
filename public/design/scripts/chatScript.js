3b2c3da45d0fbae4b1d10206ca3aac875a77cb85
MRuniversee <renatobylyk@gmail.com>
2017-08-05 22:01:08 +0300

    users update chat boxes

--- public/design/scripts/chatScript.js
+++ public/design/scripts/chatScript.js
@@ -1,5 +1,5 @@
 
-let openChats = 1;
+let openChats = [];
 let lastChatBox = '';
 
 $(document).ready(function(){
@@ -43,8 +43,8 @@ function sendText(id,event){
 
 // Open chatbox with the selected user -------------------------------------------------------------------------------------------------------
 function sendMsg(id){
-	if(lastChatBox!=id){
-		$('<div class="msg_box" id="'+id+'"style="right:'+openChats*290+'px; display:none;">'+
+	if(!checkIfOpen(id)){
+		$('<div class="msg_box" id="'+id+'"style="right:'+openChats.length*290+'px; display:none;">'+
 				'<div class="msg_head" id="'+id+'" onclick="msgHead(\''+id+'\')" >'+ id +'<div class="close" id="'+id+'" onclick="msgClose(\''+id+'\')"> X </div> </div>'+
 				'<div class="msg_wrap" id="'+id+'"><div class="msg_body"><div class="msg_push" id="'+id+'"></div></div>'+
 						'<div class="msg_footer">'+
@@ -54,7 +54,6 @@ function sendMsg(id){
 			'</div>').insertAfter('.chat_box');
 		$('.msg_wrap#'+id).show();
 		$('.msg_box#'+id).show();
-		openChats++;
 		lastChatBox = id;
 	}else{
 		$('.msg_box#'+id).show();
@@ -66,7 +65,7 @@ function sendMsg(id){
 function gotMessage(id,msg){
 //	console.log('Got a replay from '+id+' with the message :'+message);
 	if(lastChatBox!=id){
-		$('<div class="msg_box" id="'+id+'"style="right:'+openChats*290+'px; display:none;">'+
+		$('<div class="msg_box" id="'+id+'"style="right:'+openChats.length*290+'px; display:none;">'+
 				'<div class="msg_head" id="'+id+'" onclick="msgHead(\''+id+'\')" >'+ id +'<div class="close" id="'+id+'" onclick="msgClose(\''+id+'\')"> X </div> </div>'+
 				'<div class="msg_wrap" id="'+id+'"><div class="msg_body"><div class="msg_push" id="'+id+'"></div></div>'+
 						'<div class="msg_footer">'+
@@ -84,4 +83,20 @@ function gotMessage(id,msg){
 		$('<div class="msg_a">'+msg+'</div><div class="date_class">'+new Date().toUTCString() +'</div>').insertBefore('.msg_push#'+id);
 		$('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
 	}
+}
+
+
+function restructureChatBoxes(){
+
+}
+
+function checkIfOpen(name){
+	 for(let i=0 ; i <= openChats.length ; i++){
+		 if(openChats[i]===name){
+			 console.log("found him");
+			 return 1;
+		 }
+	 }
+	 openChats.push(name);
+	 return 0;
 }
