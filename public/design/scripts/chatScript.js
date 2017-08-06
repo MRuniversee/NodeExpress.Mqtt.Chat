852d7582615fc24f16f3174111117e3949c068ca
MRuniversee <renatobylyk@gmail.com>
2017-08-06 13:31:00 +0300

    chat boxes fix

--- public/design/scripts/chatScript.js
+++ public/design/scripts/chatScript.js
@@ -13,15 +13,14 @@ $(document).ready(function(){
 
 // Close chatbox with X -------------------------------------------------------------------------------------------------------
 function msgClose(id){
-	$('.msg_box#'+id).hide();
-//	if(lastChatBox==id){
-//		lastChatBox='';
-//	}
+	$('#'+id+'.msg_box').remove();
 	// TODO -- FIX the chatbox places
+	removeFromOpen(id);
+	//console.log(openChats);
 }
 // Hide chatbox ---------------------------------------------------------------------------------------------------------------
 function msgHead(id){
-	$('.msg_wrap#'+id).slideToggle('slow');
+	$('#'+id+'.msg_wrap').slideToggle('slow');
 }
 
 // Send the messaged that was typed in textarea -------------------------------------------------------------------------------------------------------
@@ -54,7 +53,6 @@ function sendMsg(id){
 			'</div>').insertAfter('.chat_box');
 		$('.msg_wrap#'+id).show();
 		$('.msg_box#'+id).show();
-		lastChatBox = id;
 	}else{
 		$('.msg_box#'+id).show();
 		msgHead(id);
@@ -64,7 +62,7 @@ function sendMsg(id){
 // Display the recieved message -------------------------------------------------------------------------------------------------
 function gotMessage(id,msg){
 //	console.log('Got a replay from '+id+' with the message :'+message);
-	if(lastChatBox!=id){
+	if(!checkIfOpen(id)){
 		$('<div class="msg_box" id="'+id+'"style="right:'+openChats.length*290+'px; display:none;">'+
 				'<div class="msg_head" id="'+id+'" onclick="msgHead(\''+id+'\')" >'+ id +'<div class="close" id="'+id+'" onclick="msgClose(\''+id+'\')"> X </div> </div>'+
 				'<div class="msg_wrap" id="'+id+'"><div class="msg_body"><div class="msg_push" id="'+id+'"></div></div>'+
@@ -75,8 +73,6 @@ function gotMessage(id,msg){
 			'</div>').insertAfter('.chat_box');
 		$('.msg_wrap#'+id).show();
 		$('.msg_box#'+id).show();
-		openChats++;
-		lastChatBox = id;
 		$('<div class="msg_a">'+msg+'</div><div class="date_class">'+new Date().toUTCString() +'</div>').insertBefore('.msg_push#'+id);
 		$('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
 	}else{
@@ -93,10 +89,14 @@ function restructureChatBoxes(){
 function checkIfOpen(name){
 	 for(let i=0 ; i <= openChats.length ; i++){
 		 if(openChats[i]===name){
-			 console.log("found him");
 			 return 1;
 		 }
 	 }
 	 openChats.push(name);
 	 return 0;
+}
+
+
+function removeFromOpen(id){
+	openChats = openChats.filter(function(e){return e!=id;});
 }
