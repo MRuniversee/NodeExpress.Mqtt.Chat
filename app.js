45c3ca96aa01a227cda7c4c1ca08e3a7a49497e9
unknown <renatobylyk@gmail.com>
2017-08-10 17:18:26 +0300

    structure change

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var counter = 0;
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public/views'));
 app.set('view engine', 'pug');
 
 
